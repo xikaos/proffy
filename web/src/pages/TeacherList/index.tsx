681e07983da55c04288d0b873f18f8de2f1b5708
Francisco Zanatta <xikaos@nyx>
2020-08-05 09:31:01 -0300

    Classes/Teacher Form structure and styles.

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,26 +4,16 @@ import './styles.css'
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da Semana</label>
-            <input type="text" id="week_day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Horário</label>
-            <input type="text" id="time" />
-          </div>
+          <Input name="subject" label="Matéria"/>
+          <Input name="week_day" label="Dia da Semana" />
+          <Input name="time" label="Hora"/>
         </form>
       </PageHeader>
       <main>
